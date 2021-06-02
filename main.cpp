07c2b31fa5bda305d1aa35a010d54171383c92af
Jean <jean.miraval@utec.edu.pe>
2021-06-01 23:26:12 -0500

    Hashing

--- main.cpp
+++ main.cpp
@@ -6,7 +6,18 @@
 #include<ctime>
 using namespace std;
 
-
+struct Bucket{
+    int Depth = 0;
+    string bucketName = "";
+    int next_1 = 0;
+    int next_2 = 0;
+    int next_overflow = 0;
+    Bucket(string currentName, int Depth, int id){
+        this->Depth = Depth;
+        this->bucketName = currentName + to_string(id);
+    };
+    Bucket(){};
+};
 
 struct Data
 {
@@ -286,38 +297,465 @@ public:
     }
 };
 
+
+
+
+
 template <typename XDT>
 class Hash : public Index<XDT> {
 public:
+      
+    string filename;
 
-    fstream fileHash;
+    Hash() {
+	filename = "HashingIndex.dat";
+        fstream fileHash;
+        this->filename = filename;
+        fileHash.open(filename, std::ios::out | std::ios::app);
+        fileHash.close();
+        fileHash.open(filename, std::ios::in | std::ios::out | ios::binary);
+        fileHash.seekg(0, ios::end);  
+        if (fileHash.tellg() == 0){
+            //Create initial Buckets - 0
+            Bucket bucket_0("", 1, 0);
+            fileHash.write((char*) &bucket_0.Depth, sizeof(bucket_0.Depth));
+            int size1 = bucket_0.bucketName.size();
+            fileHash.write((char*) &size1, sizeof(size1));
+            fileHash << bucket_0.bucketName;
+            fileHash.write((char*) &bucket_0.next_1, sizeof(bucket_0.Depth));
+            fileHash.write((char*) &bucket_0.next_2, sizeof(bucket_0.Depth));
+            fileHash.write((char*) &bucket_0.next_overflow, sizeof(bucket_0.Depth));
+            fstream bucket_temp;
+            bucket_temp.open(bucket_0.bucketName+".dat", std::ios::out | std::ios::app);
+            bucket_temp.close();
 
-    Hash(string typeIndex, string filename) : Index<XDT>(typeIndex, filename) {
-        fileHash.open(filename, std::ios::in | std::ios::out);
-
-    }
-    void readAll()
-    {
-
+            //Create initial Buckeets - 1
+            Bucket bucket_1("", 1, 1);
+            fileHash.write((char*) &bucket_1.Depth, sizeof(bucket_1.Depth));
+            int size2 = bucket_1.bucketName.size();
+            fileHash.write((char*) &size2, sizeof(size2));
+            fileHash << bucket_1.bucketName;
+            fileHash.write((char*) &bucket_1.next_1, sizeof(bucket_1.Depth));
+            fileHash.write((char*) &bucket_1.next_2, sizeof(bucket_1.Depth));
+            fileHash.write((char*) &bucket_1.next_overflow, sizeof(bucket_1.Depth));
+            fstream bucket_temp1;
+            bucket_temp1.open(bucket_1.bucketName+".dat", std::ios::out | std::ios::app);
+            bucket_temp1.close();
+        }
         fileHash.close();
     }
 
-    // read a specific record by index
-    void read(Data record)
+    string writeRecords(std::vector<XDT> records_ ,const string &outputFilename)
     {
+        try{
+            ofstream response(outputFilename, std::ios::out | std::ios::trunc);
+            for(auto&i:records_)
+            {
+                response  << i.id<<",";
+                response  << i.nombre<<",";
+                response  << i.ciclo;
+                response << '\n';
+            }
+            response.close();
+        }catch (std::ostream::failure &e) {
+            std::cerr << "Exception opening/reading/closing file\n";
+        }
+        return outputFilename;
+    }
+
+    string hash(int i){
+        string answer;
+        if(i == 1){
+            answer = "0101";
+        }else{
+            answer = "1010";
+        }
+        return answer;
+    }
+
+    void readAllBuckets()
+    {
+        ifstream fileHash;
+        fileHash.open(filename, ios::binary);
+        Bucket temp;
+        int aux;
+        while(!fileHash.eof()){
+            fileHash.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash.read(&temp.bucketName[0], aux);
+            fileHash.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            cout << temp.Depth << " " << temp.bucketName << " " << temp.next_1 << " " << temp.next_2 << " " << temp.next_overflow << "\n";
+        }
+        fileHash.close();
+    }
+
+    void readAll(){
+        vector<Data> answer;
+        ifstream fileHash;
+        fileHash.open(filename, ios::in | ios::binary);
+        Bucket temp;
+        int aux;
+        int counter1 = 0;
+        int counter2 = 0;
+        while(!fileHash.eof()){
+            counter1++;
+            fileHash.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash.read(&temp.bucketName[0], aux);
+            fileHash.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            counter2 = 0;
+            if(temp.next_1 == 0 && temp.next_2 == 0){
+                fstream bucket;
+                Data example;
+                int size1;
+                bucket.open(temp.bucketName+".dat", std::ios::in | std::ios::binary);
+                while(!bucket.eof()){
+                counter2++;
+                bucket.read((char*) &example.id, sizeof(example.id));
+                
+                bucket.read((char*) &example.ciclo, sizeof(example.ciclo));
+                
+                bucket.read((char*) &size1, sizeof(size1));
+                example.nombre.resize(size1);
+                bucket.read((char*) &example.nombre[0], size1);
+                bucket.read((char*) &example.next, sizeof(example.next));
+                if (example.ciclo != 0)
+                answer.push_back(example);
+                }
+                answer.pop_back();
+                bucket.close();
+                
+            }
+            
+        }
+        for(int i = 0; i < counter2-1; i++)
+            answer.pop_back();
+        fileHash.close();
+        return answer;
+    }
+
+    // read a specific record by index
+    void read(int index)
+    {
+        int depth = 1;
+        string key = "";
+        key+= hash(index)[depth];
+        string next_key = "" + hash(index)[depth+1];
+        ifstream fileHash;
+        Bucket temp;
+        int aux;
+        fileHash.open(filename, ios::binary);
+        while(!fileHash.eof()){
+            fileHash.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash.read(&temp.bucketName[0], aux);
+            fileHash.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            
+            
+            if(key == temp.bucketName){
+                if(next_key == "0" and &temp.next_1 != 0){
+                depth++;
+                key = key + next_key;
+                next_key = "" + hash(index)[depth-1];
+                //fileHash.seekg(temp.next_1, ios::beg);
+                }
+                else if(next_key == "1" and &temp.next_1 !=0){
+                depth++;
+                key = key + next_key;
+                next_key = hash(index)[depth+1];
+                //fileHash.seekg(temp.next_2, ios::beg);
+                }else{
+                break;
+                }
+            }   
+        }
+        fileHash.close();
+        
+        fstream bucket;
+        bucket.open(temp.bucketName+".dat", std::ios::in | std::ios::binary);
+        Data record;
+        int size1;
+        while(!bucket.eof()){
+            bucket.read((char*) &record.id, sizeof(record.id));
+            bucket.read((char*) &record.ciclo, sizeof(record.ciclo));
+            bucket.read((char*) &size1, sizeof(size1));
+            record.nombre.resize(size1);
+            bucket.read((char*) &record.nombre[0], size1);
+            bucket.write((char*) &record.next, sizeof(record.next));
+            
+            if(record.id == index){
+                cout << "id: " << record.id << "\nciclo: " << record.ciclo << "\nnombre :" << record.nombre << endl;
+                return;
+            };
+
+        }
+            bucket.close();
 
     }
 
     // insert a record by index
     void insert(Data record)
-    {
+    {   
+        //bool de necesidad de mergear
+        bool SPLIT_FLAG = false;
+
+
+        int depth = 1;
+        string key = "";
+        key+= hash(record.id)[depth];
+        string next_key = "" + hash(record.id)[depth+1];
+        cout << "For " << record.nombre << ": " << key << "\n";
+
+        ifstream fileHash;
+        fileHash.open(filename, ios::binary);
+        Bucket temp;
+        int aux;
+        int prev_bucket_position = 0;
+        
+        //Encontramos en qué bucket debe ir
+        while(!fileHash.eof()){
+            fileHash.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash.read(&temp.bucketName[0], aux);
+            fileHash.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            
+            if(key == temp.bucketName){
+                if(next_key == "0" and &temp.next_1 != 0){
+                depth++;
+                key = key + next_key;
+                cout << "\n" << "HOLA" <<key << "\n\n";
+                next_key = "" + hash(record.id)[depth-1];
+                //fileHash.seekg(temp.next_1, ios::beg);
+                }
+                else if(next_key == "1" and &temp.next_1 !=0){
+                depth++;
+                key = key + next_key;
+                next_key = hash(record.id)[depth+1];
+                //fileHash.seekg(temp.next_2, ios::beg);
+                }else{
+                break;
+                }
+                
+
+            }else{prev_bucket_position += 1;}
+            
+        }
+        fileHash.close();
+        //Escribimos en el bucket indicado
+        fstream bucket;
+        bucket.open(temp.bucketName+".dat", std::ios::out | std::ios::app);
+        bucket.write((char*) &record.id, sizeof(record.id));
+        bucket.write((char*) &record.ciclo, sizeof(record.ciclo));
+        int size1 = record.nombre.size();
+        bucket.write((char*) &size1, sizeof(size1));
+        bucket << record.nombre;
+        bucket.write((char*) &record.next, sizeof(record.next));
+        bucket.close();
+
+        
+        //Leemos el Bucket indicado para verificar el límite
+        ifstream bucket2;
+        Data aux2;
+        int counter = 0;
+        bucket2.open(temp.bucketName+".dat", std::ios::in | ios::binary);
+        int size2;
+        vector<Data> bucket_data;
+        while(!bucket2.eof()){
+                bucket2.read((char*) &aux2.id, sizeof(aux2.id));
+                cout << aux2.id << " ";
+                bucket2.read((char*) &aux2.ciclo, sizeof(aux2.ciclo));
+                cout << aux2.ciclo << " ";
+                bucket2.read((char *)&size2, sizeof(size2));
+		        aux2.nombre.resize(size2);
+		        bucket2.read(&aux2.nombre[0], size2);
+                cout << aux2.nombre << " ";
+                bucket2.read((char*) &aux2.next, sizeof(aux2.next));
+                counter++;
+                cout << counter << "\n";
+                bucket_data.push_back(aux2);
+                if(counter-1 > Bucket_size) SPLIT_FLAG = true;
+        }
+        bucket2.close();
+
+
+
+        //CUANDO SE EXCEDE LA CAPACIDAD DE UN BUCKET
+
+        if(SPLIT_FLAG == true){
+            //SI AÚN NO ALCANZAMOS EL GLOBAL DEPTH
+            if(temp.Depth <= global_Depth)
+            {
+                //CREATE NEXT TWO BUCKETS
+                string deleteName = temp.bucketName + ".dat";
+                remove(&deleteName[0]);
+                fstream nextone;
+                fstream nextzero;
+                nextzero.open(temp.bucketName+"0.dat", std::ios::out | std::ios::app);
+                nextzero.close();
+                nextone.open(temp.bucketName+"1.dat", std::ios::out | std::ios::app);
+                nextone.close();
+            }
+
+            //ESCRIBIR EN EL INDICE LOS DOS NUEVOS BUCKETS
+            ofstream fileHash2;
+            fileHash2.open(filename, std::ios::app | ios::binary);
+            Bucket bucket_0(temp.bucketName, temp.Depth+1, 0);
+            fileHash2.write((char*) &bucket_0.Depth, sizeof(bucket_0.Depth));
+            int size1 = bucket_0.bucketName.size();
+            fileHash2.write((char*) &size1, sizeof(size1));
+            fileHash2 << bucket_0.bucketName;
+            fileHash2.write((char*) &bucket_0.next_1, sizeof(bucket_0.Depth));
+            fileHash2.write((char*) &bucket_0.next_2, sizeof(bucket_0.Depth));
+            fileHash2.write((char*) &bucket_0.next_overflow, sizeof(bucket_0.Depth));
+
+            Bucket bucket_1(temp.bucketName, temp.Depth+1, 1);
+            fileHash2.write((char*) &bucket_1.Depth, sizeof(bucket_1.Depth));
+            int size2 = bucket_1.bucketName.size();
+            fileHash2.write((char*) &size2, sizeof(size2));
+            fileHash2 << bucket_1.bucketName;
+            fileHash2.write((char*) &bucket_1.next_1, sizeof(bucket_1.Depth));
+            fileHash2.write((char*) &bucket_1.next_2, sizeof(bucket_1.Depth));
+            fileHash2.write((char*) &bucket_1.next_overflow, sizeof(bucket_1.Depth));
+            fileHash2.close();
+
+            //PONER EL FLAG DE NEXT_1 Y NEXT_2 A LOS BUCKETS
+            
+            fstream fileHash3;
+            fileHash3.open(filename, ios::in | ios::out | ios::binary);
+
+            fstream falsecopy;
+            falsecopy.open("temp.dat", ios::out | ios::binary);
+
+            Bucket temp;
+            int aux;
+
+            bool flagito2 = false;
+            if(prev_bucket_position == 0){
+                fileHash3.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash3.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash3.read(&temp.bucketName[0], aux);
+            fileHash3.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash3.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash3.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            flagito2 = true;
+            }
+
+
+            while(prev_bucket_position > 0){
+            
+            fileHash3.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash3.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash3.read(&temp.bucketName[0], aux);
+            fileHash3.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash3.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash3.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            
+            falsecopy.write((char *)&temp.Depth, sizeof(temp.Depth));
+            falsecopy.write((char *)&aux, sizeof(aux));
+            falsecopy << temp.bucketName;
+            falsecopy.write((char *)&temp.next_1, sizeof(temp.next_1));
+            falsecopy.write((char *)&temp.next_2, sizeof(temp.next_2));
+            falsecopy.write((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+
+            prev_bucket_position--;
+
+            }
+            
+
+            temp.next_1 = 1;
+            temp.next_2 = 1;
+            
+            falsecopy.write((char*) &temp.Depth, sizeof(temp.Depth));
+            int size3 = temp.bucketName.size();
+            falsecopy.write((char*) &size3, sizeof(size3));
+            falsecopy << temp.bucketName;
+            falsecopy.write((char*) &temp.next_1, sizeof(temp.Depth));
+            falsecopy.write((char*) &temp.next_2, sizeof(temp.Depth));
+            falsecopy.write((char*) &temp.next_overflow, sizeof(temp.Depth));
+            bool flagito = false;
+
+            while(!fileHash3.eof()){
+            fileHash3.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash3.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash3.read(&temp.bucketName[0], aux);
+            fileHash3.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash3.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash3.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            if(flagito or flagito2){
+            falsecopy.write((char *)&temp.Depth, sizeof(temp.Depth));
+            falsecopy.write((char *)&aux, sizeof(aux));
+            falsecopy << temp.bucketName;
+            falsecopy.write((char *)&temp.next_1, sizeof(temp.next_1));
+            falsecopy.write((char *)&temp.next_2, sizeof(temp.next_2));
+            falsecopy.write((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            }
+            flagito = true;
+            }
+
+
+            fileHash3.close();
+            falsecopy.close();
+            remove("HashIndex.dat");
+            rename("temp.dat", "HashIndex.dat");
+        }
+
 
     }
 
-    // search a record by key
-    void search()
-    {
 
+
+
+    // search a record by key
+    vector<Data> search(int low, int high){
+        vector<Data> answer;
+        ifstream fileHash;
+        fileHash.open(filename, ios::binary);
+        Bucket temp;
+        int aux;
+        while(!fileHash.eof()){
+            fileHash.read((char *)&temp.Depth, sizeof(temp.Depth));
+            fileHash.read((char *)&aux, sizeof(aux));
+		    temp.bucketName.resize(aux);
+		    fileHash.read(&temp.bucketName[0], aux);
+            fileHash.read((char *)&temp.next_1, sizeof(temp.next_1));
+            fileHash.read((char *)&temp.next_2, sizeof(temp.next_2));
+            fileHash.read((char *)&temp.next_overflow, sizeof(temp.next_overflow));
+            cout << temp.Depth << " " << temp.bucketName << " " << temp.next_1 << " " << temp.next_2 << " " << temp.next_overflow << "\n";
+
+            if(temp.next_1 == 0 && temp.next_2 == 0){
+                fstream bucket;
+                Data example;
+                bucket.open(temp.bucketName+".dat", std::ios::in | std::ios::binary);
+                while(!bucket.eof()){
+                bucket.read((char*) &example.id, sizeof(example.id));
+                bucket.read((char*) &example.ciclo, sizeof(example.ciclo));
+                int size1 = example.nombre.size();
+                bucket.read((char*) &size1, sizeof(size1));
+                example.nombre.resize(size1);
+                bucket.write((char*) &example.next, sizeof(example.next));
+                bucket.close();
+                if(example.id < high && example.id > low)
+                answer.push_back(example);
+                }
+            }
+        }
+        fileHash.close();
+        return answer;
     }
 };
 
